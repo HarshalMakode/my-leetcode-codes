392ecc84ba8e81f3a2859d039f854f66baa85a39
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-08-20 19:31:26 +0530

    Time: 0 ms (100%), Space: 17.5 MB (86.46%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,21 +1,67 @@
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         vector<int> index = {-1, -1};
+
+//         for(int i = 0; i < nums.size(); i++) {
+//             if(nums[i] == target) {
+//                 index[0] = i;
+//                 break;
+//             }
+//         }
+
+//         for(int i = 0; i < nums.size(); i++) {
+//             if(nums[i] == target) {
+//                 index[1] = i;
+//             }
+//         }
+
+//         return index;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> index = {-1, -1};
+        int left = -1;
+        int right = -1;
+        int l = 0;
+        int h = nums.size() - 1;
 
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == target) {
-                index[0] = i;
-                break;
+        while(l <= h) {
+
+            int m = l + (h - l) / 2;
+
+            if(nums[m] == target) {
+                left = m;
+                h = m - 1;
+            } else if(nums[m] < target) {  
+                l = m + 1;
+            } else {
+                h = m - 1;
             }
+
         }
 
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == target) {
-                index[1] = i;
+        l = 0;
+        h = nums.size() - 1;
+
+        while(l <= h) {
+
+            int m = l + (h - l) / 2;
+
+            if(nums[m] == target) {
+                right = m;
+                l = m + 1;
+            } else if(nums[m] < target) {  
+                l = m + 1;
+            } else {
+                h = m - 1;
             }
+
         }
 
-        return index;
+        return {left, right};
     }
 };
