cca5498c0cc7113546e1c9bad2b552e026e716e3
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-08-20 18:54:10 +0530

    Time: 0 ms (100%), Space: 13.7 MB (5.93%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,15 +1,44 @@
+//linear search solution
+
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int index = -1;
+//         for(int i = 0; i < nums.size(); i++) {
+//             if(nums[i] == target) {
+//                 return i;
+//             }
+//             if(nums[i] < target) {
+//                 index = i;
+//             }
+//         }
+//         return index+1;
+//     }
+// };
+
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int index = -1;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == target) {
-                return i;
+        
+        int low = 0; 
+        int high = nums.size() - 1;
+
+        while(low <= high) {
+            
+            int mid = low + (high -low) / 2;
+                        
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-            if(nums[i] < target) {
-                index = i;
-            }
+
         }
-        return index+1;
+
+
+        return low;
     }
 };
