667c8e8a903629cc0f4593bfba05524cf87e0008
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-08-25 19:21:05 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,11 +1,74 @@
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         for(int i = 0; i< nums.size(); i++) {
+//             if(nums[i] == target) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i = 0; i< nums.size(); i++) {
-            if(nums[i] == target) {
-                return i;
+        int n = nums.size();
+        int l = 0;
+        int h = n - 1;
+
+        while(l < h) {
+
+            int m = l + (h - l) / 2;
+
+            if(nums[m] > nums[h]) {
+                l = (m+1);
+            } else {
+                h = m;
+            }
+
+        }
+
+        int start = h;
+        cout << start;
+
+        l = 0;
+        int index = -1;
+        h = start - 1; 
+
+        while(l<= h) {
+            int m = l + (h - l) / 2;
+
+            if(nums[m] == target) {
+                index = m;
+                break;
+            } else if(nums[m] < target) {
+                l = m + 1;
+            } else {
+                h = m - 1;
             }
         }
-        return -1;
+
+        if(index != -1) {
+            return index;
+        }
+
+        l = start;
+        h = n - 1;
+
+        while(l<= h) {
+            int m = l + (h - l) / 2;
+
+            if(nums[m] == target) {
+                index = m;
+                break;
+            } else if(nums[m] < target) {
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+
+        return index;
     }
 };
