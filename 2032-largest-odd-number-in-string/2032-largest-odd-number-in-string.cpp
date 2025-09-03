75c67547ccd1b9c5ae1a2e8d91e94306af4d7f1a
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-09-03 15:18:24 +0530

    Time: 0 ms (100%), Space: 17.9 MB (36.6%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -1,19 +1,34 @@
+// class Solution {
+// public:
+//     string largestOddNumber(string num) {
+//         string result = "";
+//         int n = num.length();
+
+//         for(int i = 1; i <= n; i++) {
+//             if((num[n-i] - '0') % 2 != 0) {
+//                 return num.substr(0, n- i + 1);
+//             }
+//             // int digit = num[i] - '0';
+//             // if(digit % 2 != 0) {
+//             //     result += num[i];
+//             // }
+//         }
+
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
     string largestOddNumber(string num) {
-        string result = "";
-        int n = num.length();
-
-        for(int i = 1; i <= n; i++) {
-            if((num[n-i] - '0') % 2 != 0) {
-                return num.substr(0, n- i + 1);
-            }
-            // int digit = num[i] - '0';
-            // if(digit % 2 != 0) {
-            //     result += num[i];
-            // }
+        int n= num.length();
+        for(int i=n-1;i>=0;i--){
+           if((num[i]-'0')%2 != 0)
+           {
+            return num.substr(0,i+1);
+           }
         }
-
-        return result;
+        return "";
     }
 };
