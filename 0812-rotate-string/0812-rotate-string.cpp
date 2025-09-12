3265b61596412d54a5a7cac9e69b6e26088752f7
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-09-12 16:21:55 +0530

    Time: 0 ms (100%), Space: 8.1 MB (61.79%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -0,0 +1,49 @@
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+
+//         if(s.length() != goal.length()) {
+//             return false;
+//         }
+
+//         int j = 0;
+//         char ch = s[0];
+//         bool valid = true;
+
+//         int n = goal.length();
+//         for(j = 0; j < n; j++) {
+//             if(goal[j] == ch) {
+//                 break;
+//             }
+//         }
+
+//         cout << j;
+
+//         for(int i = 0; i < s.length(); i++) {
+//             cout << s[i];
+//             cout << goal[j%n];
+//             if(s[i] != goal[j%n]) {
+//                 valid = false;
+//                 break;
+//             }
+//             j++;
+//         }
+
+//         return valid;
+//     }
+// };
+
+
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        for(int i = 0; i < s.length(); i++) {
+            rotate(s.begin(), s.begin()+1, s.end());
+            if(s == goal) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
