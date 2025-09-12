45175f670c63aad8cd8bba3994076f4148217853
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-09-12 16:29:11 +0530

    Time: 0 ms (100%), Space: 8 MB (84.19%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -35,15 +35,31 @@
 
 
 
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         for(int i = 0; i < s.length(); i++) {
+//             rotate(s.begin(), s.begin()+1, s.end());
+//             if(s == goal) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        for(int i = 0; i < s.length(); i++) {
-            rotate(s.begin(), s.begin()+1, s.end());
-            if(s == goal) {
-                return true;
-            }
+
+        if(s.length() != goal.length()) {
+            return false;
         }
+        
+        if((s+s).find(goal) != string::npos)
+            return true; 
+
         return false;
     }
 };
