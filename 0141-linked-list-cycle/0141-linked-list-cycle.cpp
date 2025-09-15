9c835e812bf9d213b19c8d396fdf84d195709223
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-09-15 18:34:28 +0530

    Time: 11 ms (39.94%), Space: 11.8 MB (79.97%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -30,18 +30,38 @@
 //     }
 // };
 
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*, bool> visited;
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         unordered_map<ListNode*, bool> visited;
 
-        while (head != nullptr) {
-            if (visited[head]) {
-                return true; 
-            }
-            visited[head] = true;
-            head = head->next;
+//         while (head != nullptr) {
+//             if (visited[head]) {
+//                 return true; 
+//             }
+//             visited[head] = true;
+//             head = head->next;
+//         }
+//         return false;
+//     }
+// };
+
+class Solution {
+        public:
+        bool hasCycle(ListNode *head) {
+
+            if (!head || !head->next) return false; // empty or single node
+
+            ListNode* slow = head;
+            ListNode* fast = head;
+
+            while (fast != nullptr && fast->next != nullptr) {
+                slow = slow->next;           // move by 1
+                fast = fast->next->next;     // move by 2
+
+                if (slow == fast) return true; // cycle detected
+                 // reached end -> no cycle
+            }    
+            return false;
         }
-        return false;
-    }
 };
