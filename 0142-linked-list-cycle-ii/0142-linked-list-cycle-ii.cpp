df4321379edba83db7ea61b75d307801503901e9
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-09-15 19:04:42 +0530

    Time: 10 ms (46.05%), Space: 11.4 MB (23.43%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,21 +6,45 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         unordered_map<ListNode*, int> mp;
+
+//         int pos = 0;
+
+//         while(head != nullptr) {
+//             if(mp.find(head) == mp.end()) {
+//                 mp[head] = pos;
+//             } else {
+//                 return head;
+//             }
+//             head = head->next;
+//             pos++;
+//         }
+//         return nullptr;
+//     }
+// };
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_map<ListNode*, int> mp;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        int pos = 0;
-
-        while(head != nullptr) {
-            if(mp.find(head) == mp.end()) {
-                mp[head] = pos;
-            } else {
-                return head;
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
             }
-            head = head->next;
-            pos++;
         }
         return nullptr;
     }
