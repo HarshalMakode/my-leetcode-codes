24b536f85cb3b630fcbd9ca9f34da18beee4d0ef
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-10-08 20:33:09 +0530

    Time: 0 ms (100%), Space: 9 MB (80.98%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -28,31 +28,69 @@
 //     }
 // };
 
-class Solution {
-    public:
-    int myAtoi(string str) {
-        long long result = 0; 
+// class Solution {
+//     public:
+//     int myAtoi(string str) {
+//         long long result = 0; 
                        
-        int i = 0;    
+//         int i = 0;    
+//         int sign = 1;
+
+//         while(i < str.length() && str[i] == ' ') {
+//             i++;
+//         }             
+        
+//         if (i < str.length() && str[i] == '-' || str[i] == '+') {
+//             sign = (str[i] == '-') ? -1 : 1;
+//             i++;
+//         }
+
+//         while (i < str.length() && str[i] >= '0' && str[i] <= '9') {
+//             result = result * 10 + str[i] - '0';
+
+//             if (sign * result >= INT_MAX) return INT_MAX;
+//             if (sign * result <= INT_MIN) return INT_MIN;
+
+//             i++;
+//         }
+//         return result * sign;
+//     }
+// };
+
+
+class Solution {
+public:
+
+    int myAtoiHelper(const string &str, int i, int sign, long long result) {
+        // Base case: end of string
+        if (i >= str.length()) return sign * result;
+
+        char c = str[i];
+
+        // Stop if non-digit
+        if (c < '0' || c > '9') return sign * result;
+
+        result = result * 10 + (c - '0');
+
+        // Handle overflow
+        if (sign * result >= INT_MAX) return INT_MAX;
+        if (sign * result <= INT_MIN) return INT_MIN;
+
+        // Recurse to next character
+        return myAtoiHelper(str, i + 1, sign, result);
+    }
+
+    int myAtoi(string str) {
+        int i = 0;
         int sign = 1;
 
-        while(i < str.length() && str[i] == ' ') {
-            i++;
-        }             
-        
-        if (i < str.length() && str[i] == '-' || str[i] == '+') {
+        while (i < str.length() && str[i] == ' ') i++;
+
+        if (i < str.length() && (str[i] == '-' || str[i] == '+')) {
             sign = (str[i] == '-') ? -1 : 1;
             i++;
         }
 
-        while (i < str.length() && str[i] >= '0' && str[i] <= '9') {
-            result = result * 10 + str[i] - '0';
-
-            if (sign * result >= INT_MAX) return INT_MAX;
-            if (sign * result <= INT_MIN) return INT_MIN;
-            
-            i++;
-        }
-        return result * sign;
+        return myAtoiHelper(str, i, sign, 0);
     }
 };
