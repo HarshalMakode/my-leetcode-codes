fd56bbfbb64dec30ab3ef61cdf4fe295219f2832
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-10-10 23:09:38 +0530

    Time: 16 ms (51.99%), Space: 29 MB (56.46%) - LeetHub

--- 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,30 +1,60 @@
+// class Solution {
+// public:
+
+//     bool stringok(string str) {
+//         for(int i = 0; i < str.length() - 1; i++) {
+//             if(str[i] != '1' && str[i+1] != '1') return false;
+//         }
+//         return true;
+//     }
+
+//     void solve(vector<string>& result, string curr, int n) {
+        
+//         if(curr.length() == n) {
+//             if(stringok(curr))
+//                 result.push_back(curr);
+//             return;
+//         }
+        
+//         solve(result, curr + '0', n);
+        
+//         solve(result, curr + '1', n);
+        
+//     }
+
+//     vector<string> validStrings(int n) {
+//         vector<string> str;
+//         solve(str, "", n);
+//         return str;
+//     }
+// };
+
+
 class Solution {
 public:
 
-    bool stringok(string str) {
-        for(int i = 0; i < str.length() - 1; i++) {
-            if(str[i] != '1' && str[i+1] != '1') return false;
-        }
-        return true;
-    }
-
-    void solve(vector<string>& result, string curr, int n) {
+    void solve(vector<string>& result, string s, int n) {
         
-        if(curr.length() == n) {
-            if(stringok(curr))
-            result.push_back(curr);
+        if(s.length() == n) {
+            result.push_back(s);
             return;
         }
         
-        solve(result, curr + '0', n);
-        
-        solve(result, curr + '1', n);
+        if (s.back() == '0') {
+            solve(result, s + '1', n);
+        } else {
+            solve(result, s + '0', n);
+            solve(result, s + '1', n);
+        }
         
     }
 
     vector<string> validStrings(int n) {
         vector<string> str;
-        solve(str, "", n);
+
+        solve(str, "0", n);
+        solve(str, "1", n);
+
         return str;
     }
 };
