b1258181bdf34743ba09db6c1854cc9515429d32
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-10-11 21:24:36 +0530

    Time: 3 ms (71.21%), Space: 13.3 MB (76.62%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -44,43 +44,78 @@
 // };
 
 
+// class Solution {
+// public:
+
+//     vector<string> result;
+
+//     bool isValid(string str) {
+//         int count = 0;
+//         for(char& ch: str) {
+//             if(ch == '(') count++;
+//             else count--;
+//             if(count < 0) return false;
+//         }
+//         return count == 0;
+//     }
+
+//     void solve(string& curr, int n) {
+        
+//         if(curr.length() == 2*n) {
+//             if(isValid(curr)) {
+//                 result.push_back(curr);
+//             }
+//             return;
+//         }
+
+//         curr.push_back('(');
+//         solve(curr, n);
+//         curr.pop_back();
+
+//         curr.push_back(')');
+//         solve(curr, n);
+//         curr.pop_back();
+//     }
+
+//     vector<string> generateParenthesis(int n) {
+//         string curr = "";
+
+//         solve(curr, n);
+
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
 
     vector<string> result;
 
-    bool isValid(string str) {
-        int count = 0;
-        for(char& ch: str) {
-            if(ch == '(') count++;
-            else count--;
-            if(count < 0) return false;
-        }
-        return count == 0;
-    }
-
-    void solve(string& curr, int n) {
-        
+    void solve(string& curr, int n, int open, int close) {
         if(curr.length() == 2*n) {
-            if(isValid(curr)) {
-                result.push_back(curr);
-            }
+            result.push_back(curr);
             return;
         }
 
-        curr.push_back('(');
-        solve(curr, n);
-        curr.pop_back();
+        if(open < n) {
+            curr.push_back('(');
+            solve(curr, n, open+1, close);
+            curr.pop_back();
+        }
 
-        curr.push_back(')');
-        solve(curr, n);
-        curr.pop_back();
+        if(close < open) {
+            curr.push_back(')');
+            solve(curr, n, open, close+1);
+            curr.pop_back();
+        }
+        
     }
 
     vector<string> generateParenthesis(int n) {
         string curr = "";
 
-        solve(curr, n);
+        solve(curr, n, 0, 0);
 
         return result;
     }
