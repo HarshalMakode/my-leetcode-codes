fc653e588825b40e481d96c3eed740c6826d84af
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-10-11 14:52:20 +0530

    Time: 114 ms (5.11%), Space: 103.8 MB (5%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    bool isValid(string s) {
+        vector<int> arr;
+        for(int i=0; i< s.size(); i++){
+            if(s[i] == '('){
+                arr.push_back(s[i]);
+            } else {
+                if (arr.empty() || 
+                    (s[i] == ')' && arr.back() != '(')) {
+                    return false;
+                }
+                arr.pop_back();
+            }
+        }
+        int result = arr.empty();
+        return result;
+    }
+
+    void generate(vector<string>& result, string s, int n) {
+
+        if(s.length() == 2*n) {
+            if (isValid(s)) {  
+                result.push_back(s);
+            }
+            return; 
+        }
+
+        generate(result, s + "(", n);
+        
+        generate(result, s + ")", n);
+        
+
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+
+        generate(result, "(", n);
+        generate(result, ")", n);
+
+        return result;
+    }
+};
