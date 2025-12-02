b9f9a839ee45a5acdf93b3c33cad81ed9ae61d2f
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-12-02 17:59:53 +0530

    Time: 17 ms (73.46%), Space: 24.1 MB (75.93%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -1,12 +1,31 @@
+// class Solution {
+// public:
+//     int maxDistinct(string s) {
+        
+//         unordered_map<char, int> mp;
+
+//         for(int i = 0; i < s.length(); i++) {
+//             mp[s[i]]++;
+//         }
+
+//         return mp.size();
+//     }
+// };
+
 class Solution {
 public:
     int maxDistinct(string s) {
-        unordered_map<char, int> mp;
+        
+        bool seen[256] = {false};
+        int count = 0;
 
-        for(int i = 0; i < s.length(); i++) {
-            mp[s[i]]++;
+        for(char c : s) {
+            if(!seen[c]) {
+                seen[c] = true;
+                count++;
+            }
         }
 
-        return mp.size();
+        return count;
     }
 };
