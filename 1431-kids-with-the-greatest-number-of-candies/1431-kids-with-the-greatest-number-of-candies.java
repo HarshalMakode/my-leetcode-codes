540208b5374c7b952ab6cd8f813952562a2935f3
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-12-18 23:05:23 +0530

    Time: 0 ms (100%), Space: 43.8 MB (56.16%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,21 +1,34 @@
+// class Solution {
+//     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+//         List<Boolean> result = new ArrayList<>();
+//         int maxi = candies[0];
+
+//         for(int i = 1; i< candies.length; i++) {
+//             if(maxi < candies[i]) {
+//                 maxi = candies[i];
+//             }
+//         }
+
+//         for(int i = 0; i< candies.length; i++) {
+//             if(maxi <= (candies[i] + extraCandies)) {
+//                 result.add(true);
+//             } else {
+//                 result.add(false);
+//             }
+//         }
+
+//         return result;
+//     }
+// }
+
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<Boolean> result = new ArrayList<>();
-        int maxi = candies[0];
+        int max = 0;
+        List<Boolean> result = new ArrayList<>(candies.length);
 
-        for(int i = 1; i< candies.length; i++) {
-            if(maxi < candies[i]) {
-                maxi = candies[i];
-            }
-        }
+        for (int i : candies) max = max < i ? i : max;
 
-        for(int i = 0; i< candies.length; i++) {
-            if(maxi <= (candies[i] + extraCandies)) {
-                result.add(true);
-            } else {
-                result.add(false);
-            }
-        }
+        for (int i : candies) result.add(i + extraCandies >= max);
 
         return result;
     }
