6ae89c42a4d725375f66673fb088d549cbc41194
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-12-19 19:35:38 +0530

    Time: 1 ms (70.28%), Space: 43.3 MB (6.06%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,21 +1,40 @@
+// class Solution {
+//     public String interpret(String command) {
+//         String str1 = "", s = "";
+
+//         for(int i = 0; i < command.length(); i++) {
+//             s = s + command.charAt(i);
+//             if(s.equals("G")) {
+//                 str1+='G';
+//                 s = "";
+//             } else if(s.equals("()")) {
+//                 str1+='o';
+//                 s = "";
+//             } else if(s.equals("(al)")) {
+//                 str1+="al";
+//                 s = "";
+//             }
+//         }
+
+//         return str1;
+//     }
+// }
+
 class Solution {
     public String interpret(String command) {
-        String str1 = "", s = "";
-
-        for(int i = 0; i < command.length(); i++) {
-            s = s + command.charAt(i);
-            if(s.equals("G")) {
-                str1+='G';
-                s = "";
-            } else if(s.equals("()")) {
-                str1+='o';
-                s = "";
-            } else if(s.equals("(al)")) {
-                str1+="al";
-                s = "";
+        StringBuilder res = new StringBuilder();
+        for(int i =0;i<command.length();i++){
+            if(command.charAt(i)=='G'){
+                res.append('G');
+            }else if(command.charAt(i)=='(' && command.charAt(i+1)==')'){
+                res.append('o');
+                i++;
+            }else if(command.charAt(i)=='(' && command.charAt(i+1)=='a'){
+                res.append("al");
+                i+=3;
             }
         }
-
-        return str1;
+        String r = res.toString();
+        return r;
     }
 }
