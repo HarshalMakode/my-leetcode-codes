39067115222466e5463d8da3557caaa06dd4cd49
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-12-21 15:33:03 +0530

    Time: 0 ms (100%), Space: 31.2 MB (79.58%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,5 +1,5 @@
 
-//linear search
+// linear search
 
 // class Solution {
 // public:
@@ -12,7 +12,6 @@
 //         return -1;
 //     }
 // };
-
 
 // class Solution {
 // public:
@@ -27,7 +26,8 @@
 
 //             if(nums[m] == target) {
 //                 return m;
-//             } else if(nums[m] < target) {    // for decending or unsorted array just change the operator to '>'
+//             } else if(nums[m] < target) {    // for decending or unsorted
+//             array just change the operator to '>'
 //                 l = m + 1;
 //             } else {
 //                 h = m - 1;
@@ -39,30 +39,30 @@
 //     }
 // };
 
-
 class Solution {
 public:
-
     int binary(vector<int>& nums, int target, int l, int h) {
-        
-        if (l > h) return -1;
+        if (l > h)
+            return -1;
 
         int m = l + (h - l) / 2;
 
-        if(nums[m] == target) {
+        if (nums[m] == target) {
             return m;
-        } else if(nums[m] > target) {
-            return binary(nums, target, l, m-1);
+        } else if (nums[m] < target) {
+            l = m + 1; // move right
         } else {
-            return binary(nums, target, m+1, h);
+            h = m - 1; // move left
         }
+
+        return binary(nums, target, l, h);
     }
 
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int l = 0;
         int h = n - 1;
-        
+
         int index = binary(nums, target, l, h);
 
         return index;
