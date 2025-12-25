89f97bf5fcb68338a0b56e29b97fb82ba2b08e9f
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-12-25 15:32:27 +0530

    Time: 162 ms (5.91%), Space: 122.2 MB (11.83%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -1,23 +1,43 @@
+// class Solution {
+//     public long maximumHappinessSum(int[] happiness, int k) {
+//         Arrays.sort(happiness);
+//         int n = happiness.length - 1; 
+//         long curr = 0;
+//         int a = 0;
+//         if(k == 1) {
+//             return (long)happiness[n];
+//         }
+
+//         for(int i = n; i >= 0; i--) {
+//             if(happiness[i] - a < 0 || k == 0) {
+//                 break;
+//             }
+//             curr += happiness[i] - a;
+//             a++;
+//             k--;
+//         }
+//         return curr;
+//     }
+// }
+
 class Solution {
     public long maximumHappinessSum(int[] happiness, int k) {
-        Arrays.sort(happiness);
-        int n = happiness.length - 1; 
-        long curr = 0;
-        int a = 0;
-        if(k == 1) {
-            return (long)happiness[n];
-        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int h : happiness)
+            pq.add(h);
 
-        for(int i = n; i >= 0; i--) {
-            if(happiness[i] - a < 0 || k == 0) {
+        long sum = 0;
+        int dec = 0;
+
+        while (k-- > 0 && !pq.isEmpty()) {
+            int top = pq.poll();
+            if (top - dec <= 0)
                 break;
-            }
-            curr += happiness[i] - a;
-            System.out.println(curr);
-            a++;
-            k--;
-
+            sum += top - dec;
+            dec++;
         }
-        return curr;
+
+        return sum;
+
     }
 }
