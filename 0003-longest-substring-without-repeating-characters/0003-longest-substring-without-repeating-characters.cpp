4d6081bbe3cefad7c811678ac24ffdedcfb0395c
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2025-12-27 00:40:42 +0530

    Time: 4 ms (71.91%), Space: 11.7 MB (63.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,31 +1,61 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int maxLength = 0;
+
+//         unordered_map<char, int> mp;
+
+//         int left = 0;
+//         int right = 0;
+
+//         int n = s.length();
+
+//         int count = 0;
+
+//         while(right < n) {
+//             if(mp.find(s[right]) == mp.end()) {
+//                 mp[s[right]]++;
+//                 count++;
+//                 right++;
+//             } else {
+//                 count = 0;
+//                 left++;
+//                 right = left;
+//                 mp.clear();
+//             }
+//             maxLength = max(maxLength, count);
+//         }
+
+//         return maxLength;
+//     }
+// };
+
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int maxLength = 0;
 
-        unordered_map<char, int> mp;
-
-        int left = 0;
-        int right = 0;
-
         int n = s.length();
 
-        int count = 0;
+        vector<int> mp(256, -1);
 
-        while(right < n) {
-            if(mp.find(s[right]) == mp.end()) {
-                mp[s[right]]++;
-                count++;
-                right++;
-            } else {
-                count = 0;
-                left++;
-                right = left;
-                mp.clear();
+        int l = 0;
+        int r = 0; 
+
+        while(r < n) {
+            if(mp[s[r]] != -1) {
+                if(mp[s[r]] >= l) {
+                    l = mp[s[r]] + 1;
+                }
             }
-            maxLength = max(maxLength, count);
-        }
+            int length = r - l + 1;
+            maxLength = max(maxLength, length);
 
+            mp[s[r]] = r;
+            r++;
+        }
+                                   
         return maxLength;
     }
 };
