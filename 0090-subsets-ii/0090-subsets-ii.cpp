0bde36c4c1707305b20c5d0bd680bdbdf655d7a4
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-01-01 23:48:45 +0530

    Time: 0 ms (100%), Space: 10.4 MB (65.73%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,28 +1,57 @@
+// class Solution {
+// public:
+
+//     void solve(vector<int>& nums, int index,  set<vector<int>>& res, vector<int>& subset) {
+//         if(index == nums.size()) {
+//             res.insert(subset);
+//             return;
+//         }
+
+//         subset.push_back(nums[index]);
+//         solve(nums, index + 1, res, subset);
+//         subset.pop_back();
+//         solve(nums, index + 1, res, subset);
+
+//     }
+
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         set<vector<int>> res;
+//         vector<int> subset;
+//         sort(nums.begin(), nums.end());
+//         solve(nums, 0, res, subset);
+//         vector<vector<int>> result;
+//         for(auto i: res) {
+//             result.push_back(i);
+//         }
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
 
-    void solve(vector<int>& nums, int index,  set<vector<int>>& res, vector<int>& subset) {
+    void solve(vector<int>& nums, int index,  vector<vector<int>>& res, vector<int>& subset) {
         if(index == nums.size()) {
-            res.insert(subset);
+            res.push_back(subset);
             return;
         }
 
         subset.push_back(nums[index]);
         solve(nums, index + 1, res, subset);
         subset.pop_back();
-        solve(nums, index + 1, res, subset);
+        int i = index+1;
+        while(i < nums.size() && nums[i] == nums[i-1]) {
+            i++;
+        }
+        solve(nums, i, res, subset);
 
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> res;
+        vector<vector<int>> res;
         vector<int> subset;
         sort(nums.begin(), nums.end());
         solve(nums, 0, res, subset);
-        vector<vector<int>> result;
-        for(auto i: res) {
-            result.push_back(i);
-        }
-        return result;
+        return res;
     }
 };
