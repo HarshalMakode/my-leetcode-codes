a185630228d538deefaf8d74fb41ff09746b9cd1
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-01-02 12:45:33 +0530

    Time: 0 ms (100%), Space: 28.4 MB (87.61%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -20,13 +20,30 @@
 //     }
 // };
 
+// //Pigeonhole Principle
+// class Solution {
+// public:
+//     int repeatedNTimes(vector<int>& A) {
+//         for (int i = 0; i < A.size() - 2; ++i)
+//             if (A[i] == A[i + 1] || A[i] == A[i + 2])
+//                 return A[i];
+//         return A.back();
+//     }
+// };
+
 
 class Solution {
 public:
-    int repeatedNTimes(vector<int>& A) {
-        for (int i = 0; i < A.size() - 2; ++i)
-            if (A[i] == A[i + 1] || A[i] == A[i + 2])
-                return A[i];
-        return A.back();
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        for(auto i: nums) {
+            mp[i]++;
+            if(mp[i] > 1) {
+                return i;
+            }
+        }
+
+        return nums[nums.size() -1];
     }
 };
