622a6e7998aecb764d2f884141bd30ba5b300c87
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-01-02 12:30:31 +0530

    Time: 0 ms (100%), Space: 28.4 MB (69.74%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -1,21 +1,32 @@
+// class Solution {
+// public:
+//     int repeatedNTimes(vector<int>& nums) {
+//         unordered_map<int, int> mp;
+//         int maxCount = 0;
+//         int maxIndex = 0;
+
+//         for(auto i: nums) {
+//             mp[i]++;
+//         }
+
+//         for(auto i: mp) {
+//             if(maxCount < i.second) {
+//                 maxCount = i.second;
+//                 maxIndex = i.first;
+//             }
+//         }
+
+//         return maxIndex;
+//     }
+// };
+
+
 class Solution {
 public:
-    int repeatedNTimes(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        int maxCount = 0;
-        int maxIndex = 0;
-
-        for(auto i: nums) {
-            mp[i]++;
-        }
-
-        for(auto i: mp) {
-            if(maxCount < i.second) {
-                maxCount = i.second;
-                maxIndex = i.first;
-            }
-        }
-
-        return maxIndex;
+    int repeatedNTimes(vector<int>& A) {
+        for (int i = 0; i < A.size() - 2; ++i)
+            if (A[i] == A[i + 1] || A[i] == A[i + 2])
+                return A[i];
+        return A.back();
     }
 };
