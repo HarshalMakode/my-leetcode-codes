696114d89881812bf5d065b33688bde44e921f75
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-01-07 00:53:05 +0530

    Time: 8 ms (24.97%), Space: 108.3 MB (90.4%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -10,45 +10,85 @@
  * };
  */
 
- //bfs
+//  //bfs
+// class Solution {
+// public:
+//     int maxLevelSum(TreeNode* root) {
+//         int maxSum = INT_MIN;
+//         int resultLevel = 0;
+
+//         queue<TreeNode*> que;
+//         que.push(root);
+
+//         int currLevel = 1;
+
+//         while(!que.empty()) {
+//             int n = que.size();
+//             int sum = 0;
+
+//             while(n--) {
+//                 TreeNode* temp = que.front();
+//                 que.pop();
+
+//                 sum += temp->val;
+
+//                 if(temp->left) {
+//                     que.push(temp->left);
+//                 }
+
+//                 if(temp->right) {
+//                     que.push(temp->right);
+//                 }
+//             }
+
+//             if(sum > maxSum) {
+//                 maxSum = sum;
+//                 resultLevel = currLevel;
+//             }
+
+//             currLevel++;
+//         }
+
+//         return resultLevel;
+//     }
+// };
+
+
+//dfs
 class Solution {
 public:
+
+    map<int, int> mp;
+
+    void DFS(TreeNode* root, int level) {
+        if(!root) 
+            return;
+
+        mp[level] += root->val;
+
+        DFS(root->left, level+1);
+        DFS(root->right, level+1);
+    }
+
     int maxLevelSum(TreeNode* root) {
+        mp.clear();
+
+        DFS(root, 1);
+
         int maxSum = INT_MIN;
         int resultLevel = 0;
 
-        queue<TreeNode*> que;
-        que.push(root);
-
-        int currLevel = 1;
-
-        while(!que.empty()) {
-            int n = que.size();
-            int sum = 0;
-
-            while(n--) {
-                TreeNode* temp = que.front();
-                que.pop();
-
-                sum += temp->val;
-
-                if(temp->left) {
-                    que.push(temp->left);
-                }
-
-                if(temp->right) {
-                    que.push(temp->right);
-                }
-            }
+        for(auto &it : mp) {
+            int level = it.first;
+            int sum = it.second;
 
             if(sum > maxSum) {
                 maxSum = sum;
-                resultLevel = currLevel;
+                resultLevel = level;
             }
-
-            currLevel++;
         }
 
         return resultLevel;
+        
     }
 };
