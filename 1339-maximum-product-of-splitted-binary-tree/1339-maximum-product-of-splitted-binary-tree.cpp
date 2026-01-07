1ae7fba1bd4dcede0057f335331acf69439f22fe
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-01-07 16:49:56 +0530

    Time: 8 ms (28.64%), Space: 78.1 MB (73.71%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    long SUM = 0;
+    long maxP = 0;
+
+    int totalSum(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        }
+
+        int leftSum = totalSum(root->left);
+        int rightSum = totalSum(root->right);
+        int sum = root->val + leftSum + rightSum;
+
+        return sum;
+    }
+
+    int find(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        }
+
+        int leftSum = find(root->left);
+        int rightSum = find(root->right);
+        long S1 = root->val + leftSum + rightSum;
+
+        long S2 = SUM - S1;
+
+        maxP = max(maxP, S1*S2);
+
+        return S1;
+
+    }
+
+    int maxProduct(TreeNode* root) {
+        if(root == NULL) {
+            return 0;
+        }
+
+        SUM = totalSum(root);
+
+        find(root);
+
+        return maxP%1000000007;
+    }
+};
