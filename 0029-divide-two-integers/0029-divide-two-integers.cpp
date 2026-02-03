dcc3419cff8aeb526e001a92f809c60ae76ef48b
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-02-03 18:56:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.63%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,9 +1,41 @@
+// class Solution {
+// public:
+//     int divide(int dividend, int divisor) {
+//         if((long)dividend/divisor == (long)INT_MAX+1) {
+//             return INT_MAX;
+//         }
+//         return (int)dividend/divisor;
+//     }
+// };
+// 
+
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if((long)dividend/divisor == (long)INT_MAX+1) {
-            return INT_MAX;
+        if(dividend == divisor) return 1;
+
+        bool sign = true;
+
+        if(dividend >= 0 && divisor < 0) sign = false;
+        if(dividend < 0 && divisor > 0) sign = false;
+        
+
+        long n = labs(dividend);
+        long d = labs(divisor);
+        long result = 0;
+
+        while(n >= d) {
+            int count = 0;
+            while(n >= (d << (count+1))) {
+                count++;
+            }
+            result += (1l << count);
+            n = n - (d * (1l << count));
         }
-        return (int)dividend/divisor;
+
+        if(result == (1l<<31) && sign) return INT_MAX;
+        if(result == (1l<<31) && !sign) return INT_MIN;
+
+        return sign ? result : -result;
     }
 };
