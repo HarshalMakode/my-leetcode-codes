b6d21010e002de25a8ae4d601a955569629d7438
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-02-12 15:26:22 +0530

    Time: 0 ms (100%), Space: 23.2 MB (82.71%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,31 +1,31 @@
-class MinStack {
-public:
-    stack<pair<int, int>> st;
+// class MinStack {
+// public:
+//     stack<pair<int, int>> st;
 
-    MinStack() {
+//     MinStack() {
         
-    }
+//     }
     
-    void push(int val) {
-        if(st.empty()) {
-            st.push({val, val});
-        } else {
-            st.push({val, min(val, st.top().second)});
-        }
-    }
+//     void push(int val) {
+//         if(st.empty()) {
+//             st.push({val, val});
+//         } else {
+//             st.push({val, min(val, st.top().second)});
+//         }
+//     }
     
-    void pop() {
-        st.pop();
-    }
+//     void pop() {
+//         st.pop();
+//     }
     
-    int top() {
-        return st.top().first;
-    }
+//     int top() {
+//         return st.top().first;
+//     }
     
-    int getMin() {
-        return st.top().second;
-    }
-};
+//     int getMin() {
+//         return st.top().second;
+//     }
+// };
 
 /**
  * Your MinStack object will be instantiated and called as such:
@@ -35,3 +35,55 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+class MinStack {
+public:
+    stack<long long> st;
+    long long mini = INT_MAX;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()) {
+            mini = val;
+            st.push(val);
+        } else {
+            if(val > mini) {
+                st.push(val);
+            } else {
+                st.push(2LL*val - mini);
+                mini = val;
+            }
+        }
+    }
+    
+    void pop() {
+        if(st.empty()) { 
+            return;
+        }
+        long long x = st.top();
+        st.pop();
+
+        if(x < mini) {
+            mini = 2*mini - x;
+        }
+    }
+    
+    int top() {
+        if(st.empty()) { 
+            return -1;
+        }
+        long long x = st.top();
+
+        if(mini < x) {
+            return x;
+        }
+        return mini;
+    }
+    
+    int getMin() {
+        return mini;
+    }
+};
