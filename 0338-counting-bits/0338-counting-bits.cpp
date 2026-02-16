4aaa261f383dc3fc7449f6ea3882211fc8204a40
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-02-16 06:42:31 +0530

    Time: 0 ms (100%), Space: 10.5 MB (97.08%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -22,13 +22,32 @@
 //     }
 // };   O(nlogn)
 
+// class Solution {
+// public:
+//     vector<int> countBits(int n) {
+//         vector<int> result(n+1);
+
+//         for (int i = 0; i <= n; i++) {
+//             result[i] = __builtin_popcount(i);
+//         }
+
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> result(n+1);
 
-        for (int i = 0; i <= n; i++) {
-            result[i] = __builtin_popcount(i);
+        result[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if(i % 2 != 0) {
+                result[i] = result[i/2] + 1;
+            } else {
+                result[i] = result[i/2];
+            }
         }
 
         return result;
