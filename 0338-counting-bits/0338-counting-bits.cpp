46fbf5f568d7903e92472eac7ebc15d76d383f5a
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-02-16 06:34:08 +0530

    Time: 2 ms (33.83%), Space: 10.7 MB (56.98%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,21 +1,34 @@
+// class Solution {
+// public:
+//     vector<int> countBits(int n) {
+//         vector<int> result(n+1);
+
+//         for (int i = 0; i <= n; i++) {
+//             int count = 0;
+//             int x = i;
+
+//             while (x > 1) {
+//                 count += x & 1;
+//                 x = x >> 1;
+//             }
+
+//             if (x == 1)
+//                 count++;
+
+//             result[i] = count;
+//         }
+
+//         return result;
+//     }
+// };   O(nlogn)
+
 class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> result(n+1);
 
         for (int i = 0; i <= n; i++) {
-            int count = 0;
-            int x = i;
-
-            while (x > 1) {
-                count += x & 1;
-                x = x >> 1;
-            }
-
-            if (x == 1)
-                count++;
-
-            result[i] = count;
+            result[i] = __builtin_popcount(i);
         }
 
         return result;
