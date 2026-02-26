18f49541e9538a636ed9a0e6eb8ce41ff0ca2d4e
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-02-26 20:17:49 +0530

    Time: 0 ms (100%), Space: 8.8 MB (83.16%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,35 +1,56 @@
+// class Solution {
+// public:
+//     void addOne(string &s) {
+//         int i = s.length() - 1;
+
+//         while(i >= 0 && s[i] != '0') {
+//             s[i] = '0';
+//             i--;
+//         }
+
+//         if(i < 0) {
+//             s = '1' + s;
+//         } else {
+//             s[i] = '1';
+//         }
+//     }
+
+//     int numSteps(string s) {
+//         int count = 0;
+
+//         while(s.length() > 1) {
+//             int n = s.length();
+
+//             if(s[n-1] == '0') {
+//                 s.pop_back();
+//             } else {
+//                 addOne(s);
+//             }
+
+//             count++;
+//         } 
+
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
-    void addOne(string &s) {
-        int i = s.length() - 1;
-
-        while(i >= 0 && s[i] != '0') {
-            s[i] = '0';
-            i--;
-        }
-
-        if(i < 0) {
-            s = '1' + s;
-        } else {
-            s[i] = '1';
-        }
-    }
-
     int numSteps(string s) {
-        int count = 0;
+        int n = s.length();
 
-        while(s.length() > 1) {
-            int n = s.length();
+        int op = 0;
+        int carry = 0;
 
-            if(s[n-1] == '0') {
-                s.pop_back();
+        for(int i = n-1; i >= 1; i--) {
+            if(((s[i] - '0') + carry) % 2 == 1) { //odd
+                op += 2;
+                carry = 1;
             } else {
-                addOne(s);
+                op += 1;
             }
+        }
 
-            count++;
-        } 
-
-        return count;
+        return op + carry;
     }
 };
