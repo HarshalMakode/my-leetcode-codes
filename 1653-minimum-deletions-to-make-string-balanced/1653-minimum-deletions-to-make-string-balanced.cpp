8ed8e967467aa5230cbe928aee22e2ba39399a74
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-02-27 13:06:29 +0530

    Time: 29 ms (43.16%), Space: 25.6 MB (46.32%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,19 +1,50 @@
+// class Solution {
+// public:
+//     int minimumDeletions(string s) {
+//         if (s.empty()) return 0;
+
+//         stack<int> st;
+//         int count = 0;
+
+//         st.push(s[0]);
+
+//         for(int i = 1; i < s.length(); i++) {
+//             if(!st.empty() && s[i] == 'a' && st.top() == 'b') {
+//                 st.pop();
+//                 count++;
+//             } else 
+//                 st.push(s[i]);
+//         }
+
+//         return count;
+//     }
+// };
+
+
 class Solution {
 public:
     int minimumDeletions(string s) {
-        if (s.empty()) return 0;
+        int n = s.length();
+        int right_a = 0;
 
-        stack<int> st;
-        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(s[i] == 'a') {
+                right_a++;
+            }
+        }
 
-        st.push(s[0]);
+        int count = INT_MAX;
+        int left_b = 0;
+        for(int i = 0; i < n; i++) {
+            if(s[i] == 'a') {
+                right_a--;
+            }
+            
+            count = min(count, right_a + left_b);
 
-        for(int i = 1; i < s.length(); i++) {
-            if(!st.empty() && s[i] == 'a' && st.top() == 'b') {
-                st.pop();
-                count++;
-            } else 
-                st.push(s[i]);
+            if(s[i] == 'b') {
+                left_b++;
+            }
         }
 
         return count;
