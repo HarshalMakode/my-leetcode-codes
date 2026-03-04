72d10f74af0d3c30f1c094e914e0f5be8091c34b
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-03-04 14:35:41 +0530

    Time: 0 ms (100%), Space: 17 MB (57.25%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,33 +1,67 @@
+// class Solution {
+// public:
+//     int numSpecial(vector<vector<int>>& mat) {
+//         int m = mat.size();
+//         int n = mat[0].size();
+
+//         int count = 0;
+
+//         for (int i = 0; i < m; i++) {
+//             for (int k = 0; k < n; k++) {
+
+//                 int countRow1 = 0;
+//                 for (int j = 0; j < n; j++) {
+//                     if (mat[i][j] == 1) {
+//                         countRow1++;
+//                     }
+//                 }
+
+//                 int countColumn1 = 0;
+//                 for (int j = 0; j < m; j++) {
+//                     if (mat[j][k] == 1) {
+//                         countColumn1++;
+//                     }
+//                 }
+
+//                 if(mat[i][k] == 1 && countRow1 == 1 && countColumn1 == 1) {
+//                     count++;
+//                 } 
+//             }
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
 
+        vector<int> countRow1(m, 0);
+        vector<int> countColumn1(n, 0);
+
+        // pre compute the count of 1
+        for (int i = 0; i < m; i++) {
+            for (int k = 0; k < n; k++) {
+                if (mat[i][k] == 1) {
+                    countRow1[i]++;
+                    countColumn1[k]++;
+                }
+            }
+        }
+
+        // now traversing once again
         int count = 0;
 
         for (int i = 0; i < m; i++) {
             for (int k = 0; k < n; k++) {
-
-                int countRow1 = 0;
-                for (int j = 0; j < n; j++) {
-                    if (mat[i][j] == 1) {
-                        countRow1++;
-                    }
-                }
-
-                int countColumn1 = 0;
-                for (int j = 0; j < m; j++) {
-                    if (mat[j][k] == 1) {
-                        countColumn1++;
-                    }
-                }
-
-                if(mat[i][k] == 1 && countRow1 == 1 && countColumn1 == 1) {
+                if (mat[i][k] == 1 && countRow1[i] == 1 && countColumn1[k] == 1) {
                     count++;
-                } 
+                }
             }
         }
+
         return count;
     }
 };
