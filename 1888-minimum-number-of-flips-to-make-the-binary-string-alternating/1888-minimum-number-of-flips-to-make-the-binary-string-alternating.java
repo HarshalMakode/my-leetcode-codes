22b51f960703976810c6e2258ffea7e8f612b068
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-03-07 13:02:44 +0530

    Time: 31 ms (28.81%), Space: 46.7 MB (94.07%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -3,21 +3,21 @@ class Solution {
 
         int n = s.length();
 
-        s = s + s;
+        //s = s + s;
 
-        StringBuilder s1 = new StringBuilder();
-        StringBuilder s2 = new StringBuilder();
+        // StringBuilder s1 = new StringBuilder();
+        // StringBuilder s2 = new StringBuilder();
 
-        for(int i = 0; i < 2 * n; i++) {
-            if(i % 2 == 1){
-                s1.append('0');
-                s2.append('1');
-            } 
-            else{
-                s1.append('1');
-                s2.append('0');
-            }
-        }
+        // for(int i = 0; i < 2 * n; i++) {
+        //     if(i % 2 == 1){
+        //         s1.append('0');
+        //         s2.append('1');
+        //     } 
+        //     else{
+        //         s1.append('1');
+        //         s2.append('0');
+        //     }
+        // }
 
         int result = Integer.MAX_VALUE;
         int flip1 = 0;
@@ -27,21 +27,27 @@ class Solution {
         int j = 0;
 
         while(j < 2 * n) {
+            
+            char s1 = (j % 2 == 1) ? '1' : '0';
+            char s2 = (j % 2 == 1) ? '0' : '1';
 
-            if(s.charAt(j) != s1.charAt(j)) {
+            if(s.charAt(j%n) != s1) {
                 flip1++;
             }
 
-            if(s.charAt(j) != s2.charAt(j)) {
+            if(s.charAt(j%n) != s2) {
                 flip2++;
             }
 
             if(j - i + 1 > n) {   // shrink window
-                if(s.charAt(i) != s1.charAt(i)) {
+                s1 = (i % 2 == 1) ? '1' : '0';
+                s2 = (i % 2 == 1) ? '0' : '1';
+
+                if(s.charAt(i%n) != s1) {
                     flip1--;
                 }
 
-                if(s.charAt(i) != s2.charAt(i)) {
+                if(s.charAt(i%n) != s2) {
                     flip2--;
                 }
 
