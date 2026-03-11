f7c40c12e58736378e4a18990db681bee98aec12
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-03-12 01:55:23 +0530

    Time: 0 ms (100%), Space: 11 MB (40.24%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,3 +1,33 @@
+// class Solution {
+// public:
+//     int characterReplacement(string s, int k) {
+//         int n = s.length();
+//         int r = 0;
+//         int l = 0;
+//         int maxi = 0;
+//         int maxf = 0;
+//         unordered_map<char, int> mp;
+
+//         while(r < n) {
+//             mp[s[r]]++;
+
+//             maxf = max(maxf, mp[s[r]]);
+
+//             while((r - l + 1) - maxf > k) {
+//                 mp[s[l]]--;
+//                 l++;
+//             }
+
+//             maxi = max(maxi, r -l + 1);
+            
+//             r++;
+//         }
+
+//         return maxi;
+//     }
+// };
+
+
 class Solution {
 public:
     int characterReplacement(string s, int k) {
@@ -6,15 +36,15 @@ public:
         int l = 0;
         int maxi = 0;
         int maxf = 0;
-        unordered_map<char, int> mp;
+        vector<int> mp(26,0);
 
         while(r < n) {
-            mp[s[r]]++;
+            mp[s[r] - 'A']++;
 
-            maxf = max(maxf, mp[s[r]]);
+            maxf = max(maxf, mp[s[r] - 'A']);
 
-            while((r - l + 1) - maxf > k) {
-                mp[s[l]]--;
+            if((r - l + 1) - maxf > k) {
+                mp[s[l] - 'A']--;
                 l++;
             }
 
