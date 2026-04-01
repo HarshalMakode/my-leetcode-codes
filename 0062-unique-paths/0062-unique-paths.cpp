8e6af62dcc5b2f1e0a0f4e1c7a6963cf7dbe416a
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-04-01 19:47:29 +0530

    Time: 0 ms (100%), Space: 8.4 MB (74.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -27,46 +27,57 @@
 // };
 
 // tabulation
+
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m, vector<int>(n, 0));
+
+//         dp[0][0] = 1;
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (i == 0 && j == 0) {
+//                     continue;
+//                 } else {
+//                     int up = 0, left = 0;
+//                     if(i > 0) up = dp[i - 1][j];
+//                     if(j > 0) left = dp[i][j - 1];
+
+//                     dp[i][j] = up + left;
+//                 }
+//             }
+//         }
+
+//         return dp[m - 1][n - 1];
+//     }
+// };
+
+//space optimization
+
 class Solution {
-public:
-    int solve(int i, int j, vector<vector<int>>& dp) {
-        if (i == 0 && j == 0) {
-            return 1;
-        }
-
-        if (i < 0 || j < 0) {
-            return 0;
-        }
-
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int up = solve(i - 1, j, dp);
-        int left = solve(i, j - 1, dp);
-
-        return dp[i][j] = up + left;
-    }
-
+    public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> dp(n, 0);
 
-        dp[0][0] = 1;
+        dp[0] = 1;
 
         for (int i = 0; i < m; i++) {
+            vector<int> temp(n, 0);
+            temp[0] = 1;
             for (int j = 0; j < n; j++) {
                 if (i == 0 && j == 0) {
                     continue;
                 } else {
                     int up = 0, left = 0;
-                    if(i > 0) up = dp[i - 1][j];
-                    if(j > 0) left = dp[i][j - 1];
+                    if(i > 0) up = dp[j];
+                    if(j > 0) left = temp[j - 1];
 
-                    dp[i][j] = up + left;
+                    temp[j] = up + left;
                 }
             }
+
+            dp = temp;
         }
 
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 };
