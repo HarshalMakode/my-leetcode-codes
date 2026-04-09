806c1573f351c66d3c69bf48bbcbf69f602b2202
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-04-09 23:06:45 +0530

    Time: 0 ms (100%), Space: 13.1 MB (55.67%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -27,37 +27,26 @@
 
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& a, vector<vector<int>>& dp) {
-        int n = a.size();
-        if(i == n - 1) 
-            return a[n - 1][j];
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int down = a[i][j] + solve(i+1, j, a, dp);
-        int digonal = a[i][j] + solve(i+1, j+1, a, dp);
-
-        return dp[i][j] = min(down, digonal);
-    }
-
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<int> front(n, 0);
         
         for(int j = 0; j < n; j++) {
-            dp[n-1][j] = triangle[n-1][j];
+            front[j] = triangle[n-1][j];
         }
 
         for(int i = n-2; i >= 0; i--) {
+            vector<int> curr(n, 0);
             for(int j = i; j >= 0; j--) {
 
-                int down = triangle[i][j] + dp[i+1][j];
-                int digonal = triangle[i][j] + dp[i+1][j+1];
+                int down = triangle[i][j] + front[j];
+                int digonal = triangle[i][j] + front[j+1];
                 
-                dp[i][j] = min(down, digonal);
+                curr[j] = min(down, digonal);
             }
+            front = curr;
         }
 
-        return dp[0][0];
+        return front[0];
     }
 };
