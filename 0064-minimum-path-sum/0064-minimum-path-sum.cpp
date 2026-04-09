c6db1d977b3ecb8923c710675607e14e9627c0f4
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-04-09 19:31:59 +0530

    Time: 3 ms (29.42%), Space: 15.9 MB (72.06%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -19,28 +19,58 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<vector<int>> dp(m, vector<int>(n, 0));
+
+//         for(int i = 0; i < m; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 if(i == 0 && j == 0) 
+//                     dp[i][j] = grid[0][0];
+//                 else {
+//                     long long up = LLONG_MAX, left = LLONG_MAX;
+
+//                     if(i > 0) up = grid[i][j] + dp[i-1][j];
+//                     if(j > 0) left = grid[i][j] + dp[i][j-1];
+
+//                     dp[i][j] = (int) min(up, left);
+//                 }
+//             }
+//         }
+
+//         return dp[m-1][n-1];
+//     }
+// };
+
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> dp(n, 0);
 
         for(int i = 0; i < m; i++) {
+            vector<int> temp(n, 0);
+
             for(int j = 0; j < n; j++) {
                 if(i == 0 && j == 0) 
-                    dp[i][j] = grid[0][0];
+                    temp[j] = grid[0][0];
                 else {
                     long long up = LLONG_MAX, left = LLONG_MAX;
 
-                    if(i > 0) up = grid[i][j] + dp[i-1][j];
-                    if(j > 0) left = grid[i][j] + dp[i][j-1];
+                    if(i > 0) up = grid[i][j] + dp[j];
+                    if(j > 0) left = grid[i][j] + temp[j-1];
 
-                    dp[i][j] = (int) min(up, left);
+                    temp[j] = (int) min(up, left);
                 }
             }
+            
+            dp = temp;
         }
 
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
