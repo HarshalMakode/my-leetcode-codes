ca1bf375fac79f0881dabaaf62b6eee23803734e
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-04-12 20:09:23 +0530

    Time: 147 ms (37.15%), Space: 17.4 MB (64.39%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,21 +1,41 @@
+// class Solution {
+// public:
+//     bool solve(int i, int sum, vector<int>& nums,  vector<vector<int>>& dp) {
+//         if(sum == 0) return true;
+//         if(i == 0) return (nums[0] == sum);
+
+//         if(dp[i][sum] != -1) return dp[i][sum];
+
+//         bool not_take = solve(i-1, sum, nums, dp);
+
+//         bool take = false;
+
+//         if(nums[i] <= sum) 
+//             take = solve(i-1, sum - nums[i], nums, dp);
+
+//         return dp[i][sum] = take | not_take;
+//     }
+
+//     bool canPartition(vector<int>& nums) {
+//         int n = nums.size();
+
+//         int sum = 0;
+//         for(int i = 0; i < n; i++) {
+//             sum += nums[i];
+//         }
+
+//         if(sum%2 != 0) return false;
+
+//         sum = sum/2;
+
+//         vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));  //plus 1 to avoid overflow
+
+//         return solve(n-1, sum, nums, dp);
+//     }
+// };
+
 class Solution {
 public:
-    bool solve(int i, int sum, vector<int>& nums,  vector<vector<int>>& dp) {
-        if(sum == 0) return true;
-        if(i == 0) return (nums[0] == sum);
-
-        if(dp[i][sum] != -1) return dp[i][sum];
-
-        bool not_take = solve(i-1, sum, nums, dp);
-
-        bool take = false;
-
-        if(nums[i] <= sum) 
-            take = solve(i-1, sum - nums[i], nums, dp);
-
-        return dp[i][sum] = take | not_take;
-    }
-
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
 
@@ -28,8 +48,25 @@ public:
 
         sum = sum/2;
 
-        vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));  //plus 1 to avoid overflow
+        vector<vector<bool>> dp(n+1, vector<bool>(sum+1, false));
 
-        return solve(n-1, sum, nums, dp);
+        for(int i = 0; i < n; i++) 
+            dp[i][0] = true;
+
+        if(nums[0] <= sum) dp[0][nums[0]] = true; 
+
+        for(int i = 1; i < n; i++) { 
+            for(int k = 1; k <= sum; k++) { 
+                bool not_take = dp[i-1][k];
+                bool take = false;
+
+                if(nums[i] <= k) 
+                    take = dp[i-1][k - nums[i]];
+
+                dp[i][k] = take | not_take;
+            } 
+        }
+
+        return dp[n-1][sum];
     }
 };
