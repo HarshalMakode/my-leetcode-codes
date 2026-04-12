1b9cc92cb52b8774fb708ae2485dca8e8f10986c
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-04-12 15:08:55 +0530

    Time: 79 ms (14.44%), Space: 20.9 MB (53.28%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,40 +1,94 @@
+// class Solution {
+// public:
+//     int m;
+//     int n;
+
+//     int solve(int i, int j1, int j2, vector<vector<int>>& grid,
+//     vector<vector<vector<int>>>& dp) {
+//         //out of bound
+//         if(j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) return -1e7; //memory out
+//         of bound
+
+//         //destination
+//         if(i == n-1) {
+//             if(j1 == j2) return grid[i][j1]; //at one cell
+//             else return grid[i][j1] + grid[i][j2]; //at different cell
+//         }
+
+//         if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+
+//         int maxi = -1e7;
+//         //explore both
+//         for(int dj1 = -1; dj1 <= 1; dj1++) {
+//             for(int dj2 = -1; dj2 <= 1; dj2++) {
+//                 if(j1 == j2)
+//                     maxi = max(maxi, grid[i][j1] + solve(i+1, j1 + dj1, j2 +
+//                     dj2, grid, dp));
+//                 else
+//                     maxi = max(maxi, grid[i][j1] + grid[i][j2] + solve(i+1,
+//                     j1 + dj1, j2 + dj2, grid, dp));
+//             }
+//         }
+
+//         return dp[i][j1][j2] = maxi;
+//     }
+
+//     int cherryPickup(vector<vector<int>>& grid) {
+//         n = grid.size(); //row
+//         m = grid[0].size(); //column
+
+//         vector<vector<vector<int>>> dp(n, vector<vector<int>>(m,
+//         vector<int>(m, -1)));
+
+//         return solve(0, 0, m - 1, grid, dp);
+//     }
+// };
+
 class Solution {
 public:
-    int m;
-    int n;
+    int m, n;
 
-    int solve(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
-        //out of bound
-        if(j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) return -1e7; //memory out of bound
+    int cherryPickup(vector<vector<int>>& grid) {
+        n = grid.size();    // row
+        m = grid[0].size(); // column
 
-        //destination
-        if(i == n-1) {
-            if(j1 == j2) return grid[i][j1]; //at one cell
-            else return grid[i][j1] + grid[i][j2]; //at different cell
-        }
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
 
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
-
-        int maxi = -1e7;
-        //explore both
-        for(int dj1 = -1; dj1 <= 1; dj1++) {
-            for(int dj2 = -1; dj2 <= 1; dj2++) {
-                if(j1 == j2) 
-                    maxi = max(maxi, grid[i][j1] + solve(i+1, j1 + dj1, j2 + dj2, grid, dp));
-                else 
-                    maxi = max(maxi, grid[i][j1] + grid[i][j2] + solve(i+1, j1 + dj1, j2 + dj2, grid, dp));
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                if (j1 == j2)
+                    dp[n - 1][j1][j2] = grid[n - 1][j1];
+                else
+                    dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
             }
         }
 
-        return dp[i][j1][j2] = maxi;
-    }
+        for (int i = n-2; i >= 0; i--) {
+            for (int j1 = 0; j1 < m; j1++) {
+                for (int j2 = 0; j2 < m; j2++) {
+                    int maxi = -1e7;
+                    for (int dj1 = -1; dj1 <= 1; dj1++) {
+                        for (int dj2 = -1; dj2 <= 1; dj2++) {
+                            int value = 0;
+                            if (j1 == j2)
+                                value = grid[i][j1];
+                            else
+                                value = grid[i][j1] + grid[i][j2];
 
-    int cherryPickup(vector<vector<int>>& grid) {
-        n = grid.size(); //row
-        m = grid[0].size(); //column
+                            if(j1+dj1 >= 0 && j1+dj1 < m && j2 + dj2 >= 0 && j2 + dj2 < m) 
+                                value += dp[i + 1][j1 + dj1][j2 + dj2];
+                            else 
+                                value += -1e7;
 
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
+                            maxi = max(maxi, value);                 
+                        }
+                    }
 
-        return solve(0, 0, m - 1, grid, dp);
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+
+        return dp[0][0][m - 1];
     }
 };
