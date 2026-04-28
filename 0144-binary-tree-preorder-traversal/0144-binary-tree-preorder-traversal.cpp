5cdc3691adeb91558ae75f9972fd5e1b4e5c2fa7
HarshalMakode <123554464+HarshalMakode@users.noreply.github.com>
2026-04-28 11:26:21 +0530

    Time: 0 ms (100%), Space: 10.8 MB (72.12%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -27,32 +27,67 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> preorder;
+
+//         if (root == NULL)
+//             return preorder;
+
+//         stack<TreeNode*> st;
+//         st.push(root);
+
+//         while (!st.empty()) {
+//             TreeNode* node = st.top();
+//             st.pop();
+
+//             preorder.push_back(node->val);
+
+//             if (node->right != NULL) {
+//                 st.push(node->right);
+//             }
+
+//             if (node->left != NULL) {
+//                 st.push(node->left);
+//             }
+//         }
+
+//         return preorder;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> preorder;
+        vector<int> inorder;
 
         if (root == NULL)
-            return preorder;
+            return inorder;
 
-        stack<TreeNode*> st;
-        st.push(root);
+        TreeNode* curr = root;
 
-        while (!st.empty()) {
-            TreeNode* node = st.top();
-            st.pop();
-
-            preorder.push_back(node->val);
-
-            if (node->right != NULL) {
-                st.push(node->right);
-            }
-
-            if (node->left != NULL) {
-                st.push(node->left);
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                inorder.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right != curr) {
+                    prev = prev->right;
+                }
+                if(prev->right == NULL) {
+                    prev->right = curr; //thread
+                    inorder.push_back(curr->val);
+                    curr = curr->left;
+                } else {
+                    prev->right = NULL; //reset thread
+                    curr = curr->right;
+                }
             }
         }
 
-        return preorder;
+        return inorder;
     }
 };
